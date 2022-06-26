6297bc19e968884ec8cac24dba08dc59b1cf6a45
Marouane Ahal <d144013152@gmail.com>
2022-06-26 01:54:42 +0100

    practice activity #4

---
+++ src/com/luv2code/springdemo/SoccerCoach.java
@@ -0,0 +1,13 @@
+package com.luv2code.springdemo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SoccerCoach implements Coach {
+
+    @Override
+    public String getDailyWorkout() {
+        return "Score 3 goals in each game";
+    }
+
+}
